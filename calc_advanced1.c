325e4f34e1740eab0a15754a8cd0f3698927ffff
Aivalio <nikosaiva06@gmail.com>
2024-10-27 11:30:55 +0200

    Advanced Calculator

---
+++ calc_advanced1.c
@@ -0,0 +1,16 @@
+/* File: calc_advanced1.c */
+#include <stdio.h>
+int main() {
+long long in1, in2, sum;
+
+printf("Εισάγετε έναν ακέραιο αριθμό:");
+scanf("%lld, &in1");
+
+printf("Εισάγετε ένα δεύτερο ακέραιο αριθμό:");
+scanf("%lld , &in2");
+
+sum= in1 + in2;
+ printf("Η πρόσθεσξ δίνει: %lld\n" , sum);
+
+return 0;
+}
