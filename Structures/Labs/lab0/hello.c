2bbb1a80f8d44ee55b77fe2ba11568fb33eb8f18
Nikos Aivaliotis <nikosaiva06@gmail.com>
2025-03-11 21:43:20 +0200

    Create hello.c

---
+++ Structures/Labs/lab0/hello.c
@@ -0,0 +1,13 @@
+/*File:hello.c*/
+//Code author: Nikos Aivaliotis
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//Main function
+int main(void) {
+
+  printf("Hello World!\n");//printf function  
+  return 0;
+
+}//Successful termination
