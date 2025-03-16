a237bd6aa1335b8b8c575c3636bb867d92683da2
Aivalio <nikosaiva06@gmail.com>
2025-03-16 12:01:29 +0200

    StackInterface.h

---
+++ Structures/Assignments/Ergasia 1/Postfixs/StackInterface.h
@@ -0,0 +1,10 @@
+/* This is the StackInterface.h file */
+#include "StackTypes.h"
+
+// Functions to handle my stack
+
+void InitializeStack(Stack *S); // Function to initialize my stack
+int Empty(Stack *S); // Function to check if my stack is empty or not
+int Full(Stack *S); // Function to check uf my stack is full or not
+void Push(ItemType X, Stack *S); // Function to push an item to mys stack
+void Pop(Stack *S, ItemType *X); // Function to remove an item from my stack
