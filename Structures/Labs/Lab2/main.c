c4cabcb708bfbfc040bbac12e3b1a0bafd8249a8
Nikos Aivaliotis <nikosaiva06@gmail.com>
2025-03-19 08:58:03 +0200

    Update main.c

--- Structures/Labs/Lab2/main.c
+++ Structures/Labs/Lab2/main.c
@@ -3,37 +3,39 @@
 #include <stdlib.h>
 #include "list.h"
 
-// Κύρια συνάρτηση για δοκιμές
 int main(void) {
-    
     List *list1 = NULL;
-    
+
+    // Προσθέτουμε στοιχεία στη λίστα
     list1 = list_prepend(list1, 3);
     list1 = list_prepend(list1, 2);
     list1 = list_prepend(list1, 1);
-    
-    print_list(list1);
-    
+
+    // Εκτυπώνουμε τη λίστα
+    printf("List 1: ");
+    list_print(list1);
+
+    // Προσθέτουμε νέο κόμβο μετά τον πρώτο
     list1 = list_add_after_first(list1, 4);
-    print_list(list1);
-    
+    printf("After adding after the first: ");
+    list_print(list1);
+
+    // Δημιουργούμε και άλλη λίστα
     List *list2 = NULL;
     list2 = list_prepend(list2, 6);
     list2 = list_prepend(list2, 5);
-    print_list(list2);
-    
+
+    // Εκτυπώνουμε τη δεύτερη λίστα
+    printf("List 2: ");
+    list_print(list2);
+
+    // Ενώνουμε τις δύο λίστες
     list1 = list_merge(list1, list2);
-    print_list(list1);
-    
-    list1 = list_append(list1, 7);
-    print_list(list1);
-    
-    list1 = list_remove_first(list1);
-    print_list(list1);
-    
-    list1 = list_remove_last(list1);
-    print_list(list1);
-    
-    free_list(list1);
+    printf("Merged List: ");
+    list_print(list1);
+
+    // Απελευθερώνουμε τη μνήμη
+    list_free(list1);
     return 0;
 }
+
