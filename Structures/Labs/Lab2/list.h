64aa0e1b742065d9018bcc730c4fbc423a1d7114
Nikos Aivaliotis <nikosaiva06@gmail.com>
2025-03-19 08:56:43 +0200

    Update list.h

--- Structures/Labs/Lab2/list.h
+++ Structures/Labs/Lab2/list.h
@@ -1,11 +1,23 @@
-/*This is the list.h file for my code*/
+/*This is the list.h file*/
+#ifndef LIST_H
+#define LIST_H
 
 typedef struct list List;
-struct list{
+struct list {
     int data;
     List *next;
 };
 
-List *list_prepend(List *l,int data);
-List *list_add_after_first(List *l,int data);
-List *list_merge(List *l1,List *l2);
+// Δηλώσεις συναρτήσεων
+List *list_prepend(List *l, int data);
+List *list_add_after_first(List *l, int data);
+List *list_merge(List *l1, List *l2);
+void list_free(List *l);
+void list_print(List *l);
+int list_size(List *l);
+int list_get_data(List *l);
+List *list_get_next(List *l);
+List *list_first(List *l);
+List *list_last(List *l);
+
+#endif
