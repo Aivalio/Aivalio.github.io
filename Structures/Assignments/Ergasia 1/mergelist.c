8639c94c8cc41bfe436d5060d3684a07b3694272
Nikos Aivaliotis <nikosaiva06@gmail.com>
2025-03-26 19:53:42 +0200

    Update mergelist.c

--- Structures/Assignments/Ergasia 1/mergelist.c
+++ Structures/Assignments/Ergasia 1/mergelist.c
@@ -1,1 +1,22 @@
 
+
+// Merging of two lists into a third one and return of a pointer to it
+ List mergeSortedLists(List l1, List l2) {
+     struct listnode dummy;
+     List tail = &dummy;
+     dummy.next = NULL;
+ 
+     while (l1 && l2) {
+         if (l1->value < l2->value) {
+             tail->next = l1;
+             l1 = l1->next;
+         } else {
+             tail->next = l2;
+             l2 = l2->next;
+         }
+         tail = tail->next;
+     }
+     tail->next = l1 ? l1 : l2;
+ 
+     return dummy.next;
+ }
