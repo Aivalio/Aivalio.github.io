c9a628ec8a1c244a3f757795b59d306db959fa9a
Aivalio <nikosaiva06@gmail.com>
2025-03-28 22:21:17 +0200

    Subsum

--- Structures/Assignments/Ergasia 1/SubSum/subsum.c
+++ Structures/Assignments/Ergasia 1/SubSum/subsum.c
@@ -17,14 +17,37 @@
  // Main function for simulation
  int main(void) {
      
-     int sumarray[] = {-76, 90, 108, 45, 76, 87}; // Define an array and initialize it with integers
-     int capacity = sizeof(sumarray) / sizeof(sumarray[0]); // The size of the array
- 
+     int*sumarray; // Declare a pointer for my array
+     int capacity; // The size of the array
+     
+     printf("Please give me the number if terms to be added: "); // Prompt the user to enter the array size
+     int result=scanf("%d\n", &capacity); // Store the input to an integer
+
+     if(result==EOF || result!=1) {
+        fprintf(stderr, "No valid input given\n");
+        return 1;
+     } // Check for valid input and an error message
+
+     sumarray=(int*)malloc(capacity*sizeof(int)); // Memory allocation for my array
+     if(sumarray==NULL) {
+        frpintf(stderr, "Memory allocation failed due to exhaustion\n");
+        return -1; // Termination of the program
+     }
+
+     for(int i=0; i<capacity; i++) {
+        scanf("%d\n", &sumarray[i]);
+     } // Filling of the array by the user
+
+     for(int i=0; i<capacity; i++) {
+        printf("%d\n", sumarray[i]);
+     } // Printing of the array
+     
      if (subsetSum(sumarray, capacity, 0))
          fprintf(stderr, "Failure\nn");
      else
          fprintf(stderr, "No set with sum equal to zero\n");
          return 1; // The program exits with code 1 and a proeper error message
  
-     return 0; // Successful program termination
+    free(sumarray); // Memory deallocation
+    return 0; // Successful program termination
  }
