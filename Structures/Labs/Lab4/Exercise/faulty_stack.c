15b2caf63961f816e81449d63075030ee038b02e
Nikos Aivaliotis <nikosaiva06@gmail.com>
2025-04-02 10:12:29 +0300

    Update faulty_stack.c

--- Structures/Labs/Lab4/Exercise/faulty_stack.c
+++ Structures/Labs/Lab4/Exercise/faulty_stack.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <limits.h>
 #include "faulty_stack.h"
-#include <stdio.h>
 
 struct node{
     int data;
@@ -24,14 +23,7 @@ Node* create_stack_node(int data){
 //Function to initialize a stack
 // ERROR HERE - INITIALIZATION ERROR
 Stack *stack_init(){
-    
     Stack *s;
-    s=malloc(sizeof(Stack));
-    if(s==NULL) {
-        fprintf(stderr, "Failed to allocate memory\n");
-        return NULL;
-    }
-
     s->list = NULL;
     s->count = 0;
     return s;
@@ -49,36 +41,33 @@ int stack_size(Stack *s){
 
 //Function to pop the top element of the stack and return it
 //ERROR HERE - MEMORY LEAK
-int stack_pop(Stack *s) { 
-    // If the stack is empty, return INT_MIN
-    if (stack_empty(s)) {
+int stack_pop(Stack *s){
+
+    //check if the stack is empty
+    if(stack_empty(s))
         return INT_MIN;
-    }
 
-    // Traverse to the last node in the stack
-    Node *cur = s->list, *prev = NULL; 
-    while (cur->next != NULL) { 
-        prev = cur; 
-        cur = cur->next; 
-    } 
+    int result;
+    if(s->count>1) {
+        Node *cur = s->list;
+        Node *prev = NULL;
 
-    // Save the data of the last node
-    int result = cur->data; 
+        while (cur->next != NULL) {
+            prev = cur;
+            cur = cur->next;
+        }
 
-    // Free the memory of the last node
-    free(cur); 
-
-    // Update the stack's list pointer
-    if (prev) {
+        result = cur->data;
         prev->next = NULL;
-    } else {
-        s->list = NULL; // If there was only one node, the stack is now empty
+    }
+    else{
+        result = s->list->data;
+        free(s->list);
+        s->list=NULL;
     }
 
-    // Decrease the element count in the stack
-    s->count--; 
-
-    return result; 
+    s->count--;
+    return result;
 }
 
 //Function to return the value of the top element of the stack without removing it
@@ -95,35 +84,31 @@ int stack_peek(Stack *s){
         cur = cur->next;
     }
 
-    return cur->data;
+    return cur->next->data;
 }
 
 //Function to insert a new element to a stack
 //ERROR HERE - INVALID READ/WRITE
 //TIP: the condition of the while loop
-Stack* stack_push(Stack *s, int data) { 
-    // Create a new node with the given data
-    Node *node = create_stack_node(data); 
-     
-    // If the stack is empty, make the new node the first element
-    if (!s->list) { 
-        s->list = node; 
-    } else { 
-        // Traverse to the last node of the stack
-        Node *cur = s->list; 
-        while(cur->next != NULL) { // Traverse until the last element
-            cur = cur->next; 
-        } 
-        // Add the new node after the last node
-        cur->next = node; // Correctly adding the new node
-    } 
-     
-    // Increment the stack's element count
-    s->count++; 
-    
-    return s; // Return the updated stack
-}
+Stack* stack_push(Stack *s,int data){
+    if(s->list==NULL){
+        s->list = create_stack_node(data);
+    }
+    else{
+        Node *node = create_stack_node(data);
+        Node *cur = s->list;
 
+        while(cur!=NULL){
+            cur = cur->next;
+        }
+
+        //Append the last node
+        cur->next = node;
+    }
+
+    s->count++;
+    return s;
+}
 
 //Function to merge two stacks. The memory of the two stacks must be freed
 //ERROR HERE - MEMORY LEAKS
@@ -141,9 +126,6 @@ Stack* stack_merge(Stack *s1,Stack *s2){
         newStack = stack_push(newStack,cur->data);
         cur = cur->next;
     }
-    
-    stack_destroy(s1);
-    stack_destroy(s2);
 
     return newStack;
 }
@@ -155,11 +137,5 @@ void stack_destroy(Stack *s){
     Node *cur = s->list;
     Node* next = NULL;
 
-    while(cur!=NULL) {
-        next=cur->next;
-        free(cur);
-        cur=next;
-    }
-    
     free(s);
 }
